30934cd4bd096537aeeef2c85b8b5f01e148fc50
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-09-06 20:47:47 +0300

    seymanurgokmen_adminpaneli_final

---
+++ seymanurgokmen_adminpaneli_final/seymanurgokmen_adminpaneli_final/PROJE/panel/anasayfa.php
@@ -0,0 +1,146 @@
+﻿<?php 
+
+ $baglan = mysqli_connect("localhost","root","","panel");
+ if(!$baglan)
+ {
+	 die("connection failed:".mysqli_connect_error());
+ }
+ else
+ {
+	 echo "";
+ }
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Teknolojik Etkinlik</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/reset.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/main.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/2col.css" title="2col" />
+<link rel="alternate stylesheet" media="screen,projection" type="text/css" href="css/1col.css" title="1col" />
+<!--[if lte IE 6]><link rel="stylesheet" media="screen,projection" type="text/css" href="css/main-ie6.css" /><![endif]-->
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/style.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/mystyle.css" />
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/switcher.js"></script>
+<script type="text/javascript" src="js/toggle.js"></script>
+<script type="text/javascript" src="js/ui.core.js"></script>
+<script type="text/javascript" src="js/ui.tabs.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".tabs > ul").tabs();
+	});
+	</script>
+</head>
+<body>
+<div id="main">
+  <!-- Tray -->
+  <div id="tray" class="box">
+    <p class="f-left box">
+      <!-- Switcher -->
+      <span class="f-left" id="switcher"> <a href="javascript:void(0);" rel="1col" class="styleswitch ico-col1" title="Display one column"><img src="design/switcher-1col.gif" alt="1 Column" /></a> <a href="javascript:void(0)" rel="2col" class="styleswitch ico-col2" title="Display two columns"><img src="design/switcher-2col.gif" alt="" /></a> </span> </p>
+
+  </div>
+  <!--  /tray -->
+  <hr class="noscreen" />
+  <!-- Menu -->
+  <div id="menu" class="box">
+    <ul class="box f-right">
+      <li><a href="giris.php"><span><strong>Giriş Yap</strong></span></a></li>
+    </ul>
+    <ul class="box">
+     <div style="margin-left:240px"> 
+	  <li><a href="anasayfa.php"><span>ANASAYFA</span></a></li>
+      <li><a href="hakkimizda.php"><span>HAKKIMIZDA</span></a></li>
+      <li><a href="kurumsal.php"><span>REFERANSLAR</span></a></li>
+      <li><a href="etkinlikler.php"><span>ETKİNLİKLER</span></a></li>
+      <li><a href="iletisim.php"><span>İLETİŞİM</span></a></li>
+	 </div>
+     
+    </ul>
+  </div>
+  <!-- /header -->
+  <hr class="noscreen" />
+  <!-- Columns -->
+  <div id="cols" class="box">
+    <!-- Aside (Left Column) -->
+    <div id="aside" class="box">
+      <div class="padding box">
+        <!-- Logo (Max. width = 200px) -->
+        <p id="logo"><a href="#"><img src="tmp/indir.jpg" alt="" /></a></p>
+       
+	 
+      </div>
+      
+    </div>
+    <!-- /aside -->
+    <hr class="noscreen" />
+    <!-- Content (Right Column) -->
+    <div id="content" class="box">
+      <h1>ANASAYFA</h1>
+      <!-- Tabs -->
+      <div class="tabs box">
+        <ul>
+          <li><a href="#tab01"><span>Lorem ipsum</span></a></li>
+          <li><a href="#tab02"><span>Lorem ipsum</span></a></li>
+          <li><a href="#tab03"><span>Lorem ipsum</span></a></li>
+        </ul>
+      </div>
+      <!-- /tabs -->
+      <!-- Tab01 -->
+      <div id="tab01">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <!-- /tab01 -->
+      <!-- Tab02 -->
+      <div id="tab02">
+        <p>Donec ornare, libero vitae facilisis molestie, mi sapien venenatis felis, sed mattis lectus nisi ac massa.</p>
+      </div>
+      <!-- /tab02 -->
+      <!-- Tab03 -->
+      <div id="tab03">
+        <p>Nam ut lorem eu orci placerat iaculis.</p>
+      </div>
+      <!-- /tab03 -->
+      <!-- 2 columns -->
+      <h3 class="tit">2 Columns (50-50)</h3>
+      <div class="col50">
+        <p class="t-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et risus. Maecenas non nunc. Proin eleifend viverra sapien. Donec id augue. Duis erat nunc, volutpat a, bibendum quis, placerat vitae, enim. Etiam consectetur, velit in viverra tempus, urna augue sollicitudin tellus, vitae interdum arcu mi at est. Donec ornare, libero vitae facilisis molestie, mi sapien venenatis felis, sed mattis lectus nisi ac massa. Cras suscipit, neque ac auctor interdum, pede nibh porta lectus, nec aliquet nulla ipsum ac nibh. Morbi feugiat ipsum id metus. In urna sapien, porttitor sed, consectetur quis, lacinia eu, ante. Donec at ipsum. Sed arcu tellus, dapibus sit amet, auctor nec, rutrum non, lacus. Nam ut lorem eu orci placerat iaculis. Proin bibendum. Suspendisse consequat.</p>
+      </div>
+      <!-- /col50 -->
+      <div class="col50 f-right">
+        <p class="t-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et risus. Maecenas non nunc. Proin eleifend viverra sapien. Donec id augue. Duis erat nunc, volutpat a, bibendum quis, placerat vitae, enim. Etiam consectetur, velit in viverra tempus, urna augue sollicitudin tellus, vitae interdum arcu mi at est. Donec ornare, libero vitae facilisis molestie, mi sapien venenatis felis, sed mattis lectus nisi ac massa. Cras suscipit, neque ac auctor interdum, pede nibh porta lectus, nec aliquet nulla ipsum ac nibh. Morbi feugiat ipsum id metus. In urna sapien, porttitor sed, consectetur quis, lacinia eu, ante. Donec at ipsum. Sed arcu tellus, dapibus sit amet, auctor nec, rutrum non, lacus. Nam ut lorem eu orci placerat iaculis. Proin bibendum. Suspendisse consequat.</p>
+      </div>
+      <!-- /col50 -->
+      <div class="fix"></div>
+      <!-- 3 columns -->
+      <h3 class="tit">3 Columns (33-33-33)</h3>
+      <div class="col33">
+        <p class="t-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et risus. Maecenas non nunc. Proin eleifend viverra sapien. Donec id augue. Duis erat nunc, volutpat a, bibendum quis, placerat vitae, enim. Etiam consectetur, velit in viverra tempus, urna augue sollicitudin tellus, vitae interdum arcu mi at est. Donec ornare, libero vitae facilisis molestie, mi sapien venenatis felis, sed mattis lectus nisi ac massa. Cras suscipit, neque ac auctor interdum, pede nibh porta lectus, nec aliquet nulla ipsum ac nibh. Morbi feugiat ipsum id metus. In urna sapien, porttitor sed, consectetur quis, lacinia eu, ante. Donec at ipsum. Sed arcu tellus, dapibus sit amet, auctor nec, rutrum non, lacus. Nam ut lorem eu orci placerat iaculis. Proin bibendum. Suspendisse consequat.</p>
+      </div>
+      <!-- /col33 -->
+      <div class="col33 center">
+        <p class="t-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et risus. Maecenas non nunc. Proin eleifend viverra sapien. Donec id augue. Duis erat nunc, volutpat a, bibendum quis, placerat vitae, enim. Etiam consectetur, velit in viverra tempus, urna augue sollicitudin tellus, vitae interdum arcu mi at est. Donec ornare, libero vitae facilisis molestie, mi sapien venenatis felis, sed mattis lectus nisi ac massa. Cras suscipit, neque ac auctor interdum, pede nibh porta lectus, nec aliquet nulla ipsum ac nibh. Morbi feugiat ipsum id metus. In urna sapien, porttitor sed, consectetur quis, lacinia eu, ante. Donec at ipsum. Sed arcu tellus, dapibus sit amet, auctor nec, rutrum non, lacus. Nam ut lorem eu orci placerat iaculis. Proin bibendum. Suspendisse consequat.</p>
+      </div>
+      <!-- /col33 -->
+      <div class="col33">
+        <p class="t-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et risus. Maecenas non nunc. Proin eleifend viverra sapien. Donec id augue. Duis erat nunc, volutpat a, bibendum quis, placerat vitae, enim. Etiam consectetur, velit in viverra tempus, urna augue sollicitudin tellus, vitae interdum arcu mi at est. Donec ornare, libero vitae facilisis molestie, mi sapien venenatis felis, sed mattis lectus nisi ac massa. Cras suscipit, neque ac auctor interdum, pede nibh porta lectus, nec aliquet nulla ipsum ac nibh. Morbi feugiat ipsum id metus. In urna sapien, porttitor sed, consectetur quis, lacinia eu, ante. Donec at ipsum. Sed arcu tellus, dapibus sit amet, auctor nec, rutrum non, lacus. Nam ut lorem eu orci placerat iaculis. Proin bibendum. Suspendisse consequat.</p>
+      </div>
+      <!-- /col33 -->
+      <div class="fix"></div>
+    </div>
+    <!-- /content -->
+  </div>
+  <!-- /cols -->
+  <hr class="noscreen" />
+  <!-- Footer -->
+  <div id="footer" class="box">
+    <p class="f-left">&copy; 2009 <a href="#">Your Company</a>, All Rights Reserved &reg;</p>
+    <p class="f-right">Templates by <a href="http://www.adminizio.com/">Adminizio</a></p>
+  </div>
+  <!-- /footer -->
+</div>
+<!-- /main -->
+</body>
+</html>
---
+++ seymanurgokmen_adminpaneli_final/seymanurgokmen_adminpaneli_final/PROJE/panel/koordinatörekle.php
@@ -0,0 +1,235 @@
+﻿<?php 
+
+ $baglan = mysqli_connect("localhost","root","","panel");
+ if(!$baglan)
+ {
+	 die("connection failed:".mysqli_connect_error());
+ }
+ else
+ {
+	 echo "";
+ }
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+
+<style>
+.divider-text {
+    position: relative;
+    text-align: center;
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+.divider-text span {
+    padding: 7px;
+    position: relative;   
+    z-index: 2;
+}
+.divider-text:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    border-bottom: 1px solid #ddd;
+    top: 55%;
+    left: 0;
+    z-index: 1;
+}
+</style>
+
+<title>Teknolojik Etkinlik</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/reset.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/main.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/2col.css" title="2col" />
+<link rel="alternate stylesheet" media="screen,projection" type="text/css" href="css/1col.css" title="1col" />
+<!--[if lte IE 6]><link rel="stylesheet" media="screen,projection" type="text/css" href="css/main-ie6.css" /><![endif]-->
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/style.css" />
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/mystyle.css" />
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/switcher.js"></script>
+<script type="text/javascript" src="js/toggle.js"></script>
+<script type="text/javascript" src="js/ui.core.js"></script>
+<script type="text/javascript" src="js/ui.tabs.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".tabs > ul").tabs();
+	});
+	</script>
+</head>
+<body>
+<div id="main">
+  <!-- Tray -->
+  <div id="tray" class="box">
+    <p class="f-left box">
+      <!-- Switcher -->
+      <span class="f-left" id="switcher"> <a href="javascript:void(0);" rel="1col" class="styleswitch ico-col1" title="Display one column"><img src="design/switcher-1col.gif" alt="1 Column" /></a> <a href="javascript:void(0)" rel="2col" class="styleswitch ico-col2" title="Display two columns"><img src="design/switcher-2col.gif" alt="" /></a> </span> </p>
+    <p class="f-right">
+	
+	<?php
+	session_start();
+	
+		$admin = $_SESSION['admin'];
+	    if(isset($_SESSION['admin'])){
+			echo "admin: ".$admin."";
+	      echo '<strong><a href="cikis.php" id="logout">Çıkış Yap</a></strong></p>';
+	      
+	   }
+	    else { header("location:giris.php"); }
+	
+	?>
+	
+  </div>
+  <!--  /tray -->
+  <hr class="noscreen" />
+  <!-- Menu -->
+  <div id="menu" class="box">
+    <ul class="box f-right">
+      <li><a href="adminsayfasi.php"><span><strong>Profilim</strong></span></a></li>
+    </ul>
+    <ul class="box">
+     <div style="margin-left:240px"> 
+	 <li><a href="anasayfa.php"><span>ANASAYFA</span></a></li>
+      <li><a href="hakkimizda.php"><span>HAKKIMIZDA</span></a></li>
+      <li><a href="kurumsal.php"><span>REFERANSLAR</span></a></li>
+      <li><a href="etkinlikler.php"><span>ETKİNLİKLER</span></a></li>
+      <li><a href="iletisim.php"><span>İLETİŞİM</span></a></li>
+	 </div>
+     
+    </ul>
+  </div>
+  <!-- /header -->
+  <hr class="noscreen" />
+  <!-- Columns -->
+  <div id="cols" class="box">
+    <!-- Aside (Left Column) -->
+    <div id="aside" class="box">
+      <div class="padding box">
+        <!-- Logo (Max. width = 200px) -->
+        <p id="logo"><a href="#"><img src="tmp/indir.jpg" alt="" /></a></p>
+       
+	   
+        <!-- Create a new project -->
+        <p id="btn-create" class="box"><a href="etkinlikekle.php"><span>Yeni Etkinlik ekle</span></a></p>
+		<p id="btn-create" class="box"><a href="etkinlikduzenle.php"><span>Etkinlik Düzenle/sil</span></a></p>
+		<p id="btn-create" class="box"><a href="koordinatörekle.php"><span>Koordinatör ekle</span></a></p>
+		<p id="btn-create" class="box"><a href="koordinatorduzenle.php"><span>Koordinatör Düzenle/sil</span></a></p>
+      </div>
+      
+    </div>
+    <!-- /aside -->
+    <hr class="noscreen" />
+    <!-- Content (Right Column) -->
+    <div id="content" class="box">
+      <h1>Etkinlik Koordinatörü ekle</h1>
+
+     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+
+
+<div class="container">
+
+<div class="card bg-light">
+<article class="card-body mx-auto" style="max-width: 400px;">
+	<h4 class="card-title mt-3 text-center">Koordinatör Bilgileri</h4> <hr>
+	<form method="post" action="">
+	<div class="form-group input-group">
+		<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		 </div>
+        <input name="adsoyad" class="form-control" placeholder="Ad Soyad" type="text">
+    </div> <!-- form-group// -->
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+		 </div>
+        <input name="email" class="form-control" placeholder="Email" type="email">
+    </div> <!-- form-group// -->
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-phone"></i> </span>
+		</div>
+    	<input name="telefon" class="form-control" placeholder="Telefon Numarası" type="text">
+    </div> <!-- form-group// -->
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		</div>
+		<select class="form-control" name="unvan">
+		    <option value="0">- Seçiniz -</option>
+			<option value="Genel Koordinatör">Genel Koordinatör</option>
+			<option value="Sosyal Aktiviteler Sorumlusu">Sosyal Aktiviteler Sorumlusu</option>
+			<option value="İç İlişkiler Sorumlusu">İç İlişkiler Sorumlusu</option>
+			<option value="Dış İlişkiler Sorumlusu">Dış İlişkiler Sorumlusu</option>
+			<option value="Teknoloji ve Grafik Sorumlusu">Teknoloji ve Grafik Sorumlusu</option>
+		</select>
+	</div> <!-- form-group end.// -->
+                                         
+      <div class="form-group">
+        <button type="submit" name="kaydet" class="btn btn-primary btn-block">KAYDET</button>
+    </div> <!-- form-group// --> 
+     <?php 
+   $adsoyad = trim($_POST["adsoyad"]);
+   $email = trim($_POST["email"]);
+   $telefon = trim($_POST["telefon"]);
+   $unvan = trim($_POST["unvan"]);
+  
+		 if(isset($_POST["kaydet"]))
+	    {
+			if (filter_var($email, FILTER_VALIDATE_EMAIL))
+			{
+              echo '<p style="color:red"> GEÇERSİZ E-MAİL ADRESİ ! </p>';
+
+			}
+		else{
+			   $sorgu = "insert into koordinatorler(adsoyad,email,telefon,unvan) values ('".$adsoyad."','".$email."','".$telefon."','".$unvan."')";
+		      $add = mysqli_query($baglan,$sorgu);
+		      if($add)
+		      {
+			   echo '<p style="color:green">TEBRİKLER ! KAYDINIZ BAŞARIYLA TAMAMLANDI...</p>';
+	        
+		      }
+		      else
+		      {
+			   echo '<p style="color:red"> HATA ! KAYIT İŞLEMİ GERÇEKLEŞTİRİLEMEDİ...</p>';
+		       }
+		}
+	   
+	    }
+		else
+		{
+			echo '';
+		} 
+?>
+	
+</form>
+</article>
+</div> <!-- card.// -->
+
+</div> 
+<!--container end.//-->
+
+
+<br><br>
+</article>
+    </div>
+    <!-- /content -->
+  </div>
+  <!-- /cols -->
+  <hr class="noscreen" />
+  <!-- Footer -->
+  <div id="footer" class="box">
+    <p class="f-left">&copy; 2009 <a href="#">Your Company</a>, All Rights Reserved &reg;</p>
+    <p class="f-right">Templates by <a href="http://www.adminizio.com/">Adminizio</a></p>
+  </div>
+  <!-- /footer -->
+</div>
+<!-- /main -->
+</body>
+</html>
