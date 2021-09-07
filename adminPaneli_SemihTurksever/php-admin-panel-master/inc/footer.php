27e952ae58b512f1f0849f7fa46f76bd7b31e726
Erkut <74938334+erkuttbulutt@users.noreply.github.com>
2021-09-08 00:58:35 +0300

    adminPaneli_SemihTurksever

---
+++ adminPaneli_SemihTurksever/php-admin-panel-master/inc/footer.php
@@ -0,0 +1,27 @@
+﻿
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="http://i.po.st/static/v3/post-widget.js#publisherKey=ree2tvdnq33tbm2u2lfh" type="text/javascript"></script>
+
+	 <section class="page-section cta">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-9 mx-auto">
+					<div id="top-bar"> 
+					<div style="float: left; margin-left: 300px;margin-top: -10px;"> 
+					<a href="https://www.facebook.com/semih.turksever.9/" title="Facebook" alt="Facebook"><img width="50" height="50" src="logo/facebook.png"></a>
+					<a href="https://twitter.com/smhtrksvr" title="Twitter" alt="Twitter"><img width="50" height="50" src="logo/twitter.png"></a>
+					<a href="https://mail.google.com/mail/u/0/#inbox?compose=new" title="Google" alt="Google"><img width="50" height="50" src="logo/google.png"></a>
+					<a href="https://www.linkedin.com/in/semih-t%C3%BCrksever/" title="Linkedin" alt="Linkedin"><img width="50" height="50" src="logo/linkedin.png"></a>
+					<a href="https://www.youtube.com/c/BMDersleri" title="Youtube" alt="Youtube"><img width="50" height="50" src="logo/youtube.png"></a>
+					</div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+
+</html>
---
+++ adminPaneli_SemihTurksever/php-admin-panel-master/inc/nav.php
@@ -0,0 +1,36 @@
+﻿<body>
+<!--
+<h1 class="site-heading text-center text-white d-none d-lg-block">
+</h1>
+-->
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item px-lg-4 <?php echo $sayfa == 'Ana Sayfa' ? 'active' : '' ?>">
+                    <a class="nav-link text-uppercase text-expanded" href="index.php">Ana Sayfa
+
+                    </a>
+                </li>
+                <li class="nav-item px-lg-4 <?php echo $sayfa == 'Hakkımızda' ? 'active' : '' ?>">
+                    <a class="nav-link text-uppercase text-expanded" href="hakkimizda.php">Hakkımda</a>
+                </li>
+                <li class="nav-item px-lg-4 <?php echo $sayfa == 'Fotoğraflar' ? 'active' : '' ?>">
+                    <a class="nav-link text-uppercase text-expanded" href="fotograf.php">Fotoğraflarım</a>
+                </li>
+                <li class="nav-item px-lg-4 <?php echo $sayfa == 'İletişim' ? 'active' : '' ?>">
+                    <a class="nav-link text-uppercase text-expanded" href="iletisim.php">İLETİŞİM</a>
+                </li>
+				<li class="nav-item px-lg-4 <?php echo $sayfa == 'Yönetim' ? 'active' : '' ?>">
+                    <a class="nav-link text-uppercase text-expanded" href="/admin/index.php">YÖNETİM</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
